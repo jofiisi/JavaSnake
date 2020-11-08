9964c47bb7b24801176611108bb623b082b6cd68
jofiisi <73760260+jofiisi@users.noreply.github.com>
2020-11-08 20:11:11 +0100

    Basic

--- src/Action/Main.java
+++ src/Action/Main.java
@@ -1,9 +1,11 @@
 package Action;
+import gui.Gui;
 
 public class Main {
 
     public static void main(String[] args)
     {
-        System.out.print("Das ist ein Github Test");
+        Gui g = new Gui();
+        g.create();
     }
 }
---
+++ src/gui/Draw.java
@@ -0,0 +1,31 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Draw extends JLabel
+{
+    protected void  paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2d =(Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+
+        g.setColor(Color.LIGHT_GRAY);
+        g.fillRect(0, 0, Gui.width, Gui.height);
+
+        g.setColor(Color.GRAY);
+        for(int x = 0; x < 16; x++)
+        {
+            for (int y = 0; y < 16; y++)
+            {
+                g.drawRect(x*32 + Gui.xoff, y * 32 + Gui.yofff, 32 , 32);
+            }
+        }
+
+        g.setColor(Color.BLACK);
+        g.drawRect(Gui.xoff, Gui.yofff, 512, 512);
+
+        repaint();
+    }
+}
