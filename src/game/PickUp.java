b476ef666c29193024953049e463e986e7ca1c9a
jofiisidkt <73963808+jofiisidkt@users.noreply.github.com>
2020-11-18 13:53:58 +0100

    Update PickUp.java

--- src/game/PickUp.java
+++ src/game/PickUp.java
@@ -1,6 +1,21 @@
 package game;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class PickUp
 {
     int x,y;
+
+    public PickUp()
+    {
+        this.x = ThreadLocalRandom.current().nextInt(0, 15);
+        this.y = ThreadLocalRandom.current().nextInt(0, 15);
+    }
+    public void reset()
+    {
+        this.x = ThreadLocalRandom.current().nextInt(0, 15);
+        this.y = ThreadLocalRandom.current().nextInt(0, 15);
+    }
+
+
 }
